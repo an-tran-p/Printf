1efb7e07e7a306b55716f8a834d64b359184f63c
An Tran <atran@c1r4p1.hive.fi>
2024-11-14 12:12:11 +0200

    add fixes

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,11 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:11:25 by atran             #+#    #+#             */
-/*   Updated: 2024/11/13 22:01:15 by atran            ###   ########.fr       */
+/*   Updated: 2024/11/14 12:00:22 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 int	ft_putchar(char c)
 {
@@ -23,6 +23,11 @@ int	ft_putstr(char *s)
 	int	i;
 
 	i = 0;
+	if (!s)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
 	while (s[i] != '\0')
 	{
 		write(1, &s[i], 1);
@@ -31,7 +36,7 @@ int	ft_putstr(char *s)
 	return (i);
 }
 
-int	ft_specifier(va_list args, char c, int count)
+int	ft_specifier(va_list args, va_list args_cpy, char c, int count)
 {
 	if (c == 'c')
 		count += ft_putchar((char)va_arg(args, int));
@@ -47,8 +52,13 @@ int	ft_specifier(va_list args, char c, int count)
 		count += ft_unsigned_hex(va_arg(args, unsigned int), c);
 	if (c == 'p')
 	{
-		count += ft_putstr("0x");
-		count += ft_unsigned_hex(va_arg(args, unsigned long int), c);
+		if (va_arg(args_cpy, unsigned long int) != 0)
+		{
+			count += ft_putstr("0x");
+			count += ft_unsigned_hex(va_arg(args, unsigned long int), c);
+		}
+		else
+			count += ft_putstr("(nil)");
 	}
 	return (count);
 }
@@ -57,20 +67,27 @@ int	ft_printf(const char *str, ...)
 {
 	va_list	args;
 	int		count;
+	va_list	args_cpy;
 
 	count = 0;
+	va_copy(args, args_cpy);
 	va_start(args, str);
+	va_start(args_cpy, str);
 	while (*str != '\0')
 	{
 		if (*str == '%')
 		{
 			str++;
-			count = ft_specifier(args, *str, count);
+			count = ft_specifier(args, args_cpy, *str, count);
 			str++;
 		}
-		count += ft_putchar(*str);
-		str++;
+		if (*str != '\0' && *str != '%')
+		{
+			count += ft_putchar(*str);
+			str++;
+		}
 	}
 	va_end(args);
+	va_end(args_cpy);
 	return (count);
 }
--- main.c
+++ main.c
@@ -6,42 +6,67 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 21:40:56 by atran             #+#    #+#             */
-/*   Updated: 2024/11/13 22:01:18 by atran            ###   ########.fr       */
+/*   Updated: 2024/11/14 12:00:47 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "printf.h"
+#include "ft_printf.h"
 
 int	main(void)
 {
+	int	a;
+	int	b;
 	//test %d, %i, %s, %c & %%
-	int	a = printf("Hello %s we are %%Hiver %d %c\n", "Its purpose is to set the stage and define which elements will be stable and which will vary. This is when your va_list variable will have all the elements in the table.", -756788666, '!');
-	int	b = ft_printf("Hello %s we are %%Hiver %d %c\n", "Its purpose is to set the stage and define which elements will be stable and which will vary. This is when your va_list variable will have all the elements in the table.", -756788666, '!');
+	a = printf("Hello %s we are %%Hiver %d %c\n", "Its purpose is to set the stage and define which elements will be stable and which will vary. This is when your va_list variable will have all the elements in the table.", -756788666, '!');
+	b = ft_printf("Hello %s we are %%Hiver %d %c\n", "Its purpose is to set the stage and define which elements will be stable and which will vary. This is when your va_list variable will have all the elements in the table.", -756788666, '!');
 	printf("%d %d\n", a, b);
 
-	int	c = printf("Let's buy %d %s today because we have %d people coming %c\n", 7, "chicken legs", 7, '!');
-	int	d = ft_printf("Let's buy %d %s today because we have %d people coming %c\n", 7, "chicken legs", 7, '!');
-	printf("%d %d\n", c, d);
+	a = printf("Let's buy %d %s today because we have %d people coming %c\n", 7, "chicken legs", 7, '!');
+	b = ft_printf("Let's buy %d %s today because we have %d people coming %c\n", 7, "chicken legs", 7, '!');
+	printf("%d %d\n", a, b);
+
+	//test specifier as the last character of the str
+	a = printf("%c", '0');
+	printf("\n");
+	b = ft_printf("%c", '0');
+	printf("\n");
+	printf("%d %d\n", a, b);
+
+	a = printf(" %c", '0' - 256);
+	printf("\n");
+	b = ft_printf(" %c", '0' - 256);
+	printf("\n");
+	printf("%d %d\n", a, b);
 
 	//test %u
-	int	e = printf("%u, %u, %u\n", -23455667, 0, 425374947);
-	int	f = ft_printf("%u, %u, %u\n",  -23455667, 0, 425374947);
-	printf("%d %d\n", e, f);
+	a = printf("%u, %u, %u\n", -23455667, 0, 425374947);
+	b = ft_printf("%u, %u, %u\n",  -23455667, 0, 425374947);
+	printf("%d %d\n", a, b);
 
 	//test %x & %X
-	int g = printf("%x, %x, %X, %X, %X\n", 10, -10, -3456789, 34567, 0);
-	int h = ft_printf("%x, %x, %X, %X, %X\n", 10, -10, -3456789, 34567, 0);
-	printf("%d %d\n", g, h);
+	a = printf("%x, %x, %X, %X, %X\n", 10, -10, -3456789, 34567, 0);
+	b = ft_printf("%x, %x, %X, %X, %X\n", 10, -10, -3456789, 34567, 0);
+	printf("%d %d\n", a, b);
 
 	//test %p
 	char ch;
-	int i= printf("%p\n", &ch);
-	int k= ft_printf("%p\n", &ch);
-	printf("%d %d\n", i, k);
+	a = printf("%p\n", &ch);
+	b = ft_printf("%p\n", &ch);
+	printf("%d %d\n", a, b);
 
 	char *test_str;
-	int l= printf("%p\n", &test_str);
-	int m= ft_printf("%p\n", &test_str);
-	printf("%d %d\n", l, m);
+	a = printf("%p\n", &test_str);
+	b = ft_printf("%p\n", &test_str);
+	printf("%d %d\n", a, b);
+
+	//test empty pointer passed in as parameter
+	a = printf("Hello %p\n", (void *)0);
+	b = ft_printf("Hello %p\n", (void *)0);
+	printf("%d %d\n", a, b);
+
+	//test empty str passed in as parameter
+	a = printf(" NULL %s NULL \n", (char *)NULL);
+	b = ft_printf(" NULL %s NULL \n", (char *)NULL);
+	printf("%d %d\n", a, b);
 }
